be6380b6ae8d8fc9369e69cb5d2b2ca598e42df7
HackISTP <hyeminpak@gmail.com>
2023-02-13 23:40:24 +0900

    react 개념정리

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name = "hyemin";
+  const list = ["우유", "딸기", "초코"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="orange">{`Hello ${name}`}</h1>
+      <h2>Hello</h2>
+      <h3>{name}</h3>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
