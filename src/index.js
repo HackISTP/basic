aae3ae92fee1b40134bc2ff107f7bb6c20c41035
HackISTP <hyeminpak@gmail.com>
2023-02-17 02:16:22 +0900

    useContext 으로 필요한 곳만 렌더링 되도록 사용해보기

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import AppForm from './AppForm';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import AppTheme from "./AppTheme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppForm />
+    <AppTheme />
   </React.StrictMode>
 );
 
